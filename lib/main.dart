55069ecc67e42e7bc1ca558f656a6ab1d6891ac6
Amal Krishna <amalkrishnajk@gmail.com>
2022-07-05 13:17:37 +0530

    Adding provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
-
+import 'package:go_todo/StateManagement/provider1.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_todo/navigation_screen.dart';
 
@@ -13,50 +14,55 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Go-Todo',
-      themeMode: ThemeMode.system,
-      darkTheme: ThemeData(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> DataStateProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Go-Todo',
+        themeMode: ThemeMode.system,
+        darkTheme: ThemeData(
 
-        secondaryHeaderColor: Color(0xff1E1E1E),//secondary backgroud color
-        iconTheme: IconThemeData(
-          color: Colors.white54
-        ),
-        textTheme: TextTheme(
-          subtitle1: TextStyle(
+          secondaryHeaderColor: const Color(0xff1E1E1E),//secondary backgroud color
+          iconTheme: const IconThemeData(
             color: Colors.white54
           ),
-
-        ),
-        backgroundColor: Color(0xff101010),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xff272727)
-        ),
-        scaffoldBackgroundColor: Color(0xff101010),
-
-      ),
-
-      //................................................................................................................................
-      theme: ThemeData(
-
-        secondaryHeaderColor: Color(0xffF4F8FF),//secondary background color
-        iconTheme: IconThemeData(
-          color: Color(0xff888888)
-        ),
-        textTheme: TextTheme(
+          textTheme: const TextTheme(
             subtitle1: TextStyle(
-                color: Color(0xff222222)
-            )
-        ),
-        scaffoldBackgroundColor:Color(0xffFDFDFD),
-        backgroundColor:Color(0xffFDFDFD) ,
-        appBarTheme: AppBarTheme(
-          color: Color(0xff3E6FF5)
+              color: Colors.white54
+            ),
+
+          ),
+          backgroundColor: const Color(0xff101010),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color(0xff272727)
+          ),
+          scaffoldBackgroundColor: const Color(0xff101010),
+
         ),
 
-        //primarySwatch: Colors.blue,
+        //................................................................................................................................
+        theme: ThemeData(
+
+          secondaryHeaderColor: const Color(0xffF4F8FF),//secondary background color
+          iconTheme: const IconThemeData(
+            color: Color(0xff888888)
+          ),
+          textTheme: const TextTheme(
+              subtitle1: TextStyle(
+                  color: Color(0xff222222)
+              )
+          ),
+          scaffoldBackgroundColor:const Color(0xffFDFDFD),
+          backgroundColor: const Color(0xffFDFDFD) ,
+          appBarTheme: const AppBarTheme(
+            color: Color(0xff3E6FF5)
+          ),
+
+          //primarySwatch: Colors.blue,
+        ),
+        home: const NavigationScreen(),
       ),
-      home: const NavigationScreen(),
     );
   }
 }
